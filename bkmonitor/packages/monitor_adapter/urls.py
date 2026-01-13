7661cef3426db84fa1f7d7ae00cce6c19afe46ea
v_wypgwu <v_wypgwu@tencent.com>
2026-01-13 10:19:19 +0800

    feat: 调整监控代码

--- bkmonitor/packages/monitor_adapter/urls.py
+++ bkmonitor/packages/monitor_adapter/urls.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Tencent is pleased to support the open source community by making 蓝鲸智云 - 监控平台 (BlueKing - Monitor) available.
 Copyright (C) 2017-2021 THL A29 Limited, a Tencent company. All rights reserved.
@@ -8,6 +7,7 @@ Unless required by applicable law or agreed to in writing, software distributed 
 an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 specific language governing permissions and limitations under the License.
 """
+
 from blueapps.account.decorators import login_exempt
 from django.http import HttpResponse
 from django.urls import include, re_path
@@ -24,7 +24,7 @@ urlpatterns = [
     re_path(r"^", include("monitor_adapter.home.urls")),
     re_path(r"^", include("monitor_adapter.api.urls")),
     re_path(r"^grafana/", include("monitor_adapter.grafana.urls")),
-    re_path(r"^", include("healthz.urls")),
-    re_path(r"^rest/v1/", include("healthz.urls")),
+    # re_path(r"^", include("healthz.urls")),
+    # re_path(r"^rest/v1/", include("healthz.urls")),
     re_path(r"^ping$", ping),
 ]
