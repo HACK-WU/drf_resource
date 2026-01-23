fbfe4f3c0f3ed781d7fe834ac718b0972ea7d0b7
HACK-WU <v_wypgwu@tencent.com>
2026-01-23 11:48:20 +0800

    feat: 新增urls文件

--- bkmonitor/urls.py
+++ bkmonitor/urls.py
@@ -90,11 +90,8 @@ if settings.API_SUB_PATH:
 if settings.ENVIRONMENT != "production":
     from django.urls import path
 
-    # 导入 API explorer views
-    from drf_resource.api_explorer.urls import urlpatterns as api_explorer_urlpatterns
-
-    urlpatterns += api_explorer_urlpatterns
     urlpatterns += [
+        path("", include("drf_resource.urls")),
         re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
         re_path(r"^swagger/$", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
         re_path(r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
